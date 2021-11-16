26b20b419d66efb0a44dabe376cf94e094684479
Johan <johan@appliedtechnology.se>
2021-11-16 14:51:33 +0100

    static content

--- functions/hellosalt.js
+++ functions/hellosalt.js
@@ -1,5 +1,5 @@
 exports.handler = async function(event, context) {
-    console.log(event, context);
+    console.log(event.httpMethod, context);
     return {
         statusCode:200,
         body: JSON.stringify({message: "Hello Fall-21"})
